7d1b9b0e9e1bb83c3cbf8610a0b00e2086e21a40
Maëllie <maellie.brient-bert@epitech.eu>
2024-04-02 15:59:15 +0200

    [ADD] fonction str_isalpha

---
+++ libc/my_str_isalpha.c
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2023
+** delivery
+** File description:
+** my_str_isalpha
+*/
+
+int my_str_isalpha(char const *str)
+{
+    int index = 0;
+
+    while (str[index] != '\0') {
+        if (str[index] >= 48 && str[index] <= 57) {
+                return 1;
+        } 
+        if (str[index] >= 97 && str[index] <= 122) {
+            return 1;
+        } 
+        if (str[index] >= 65 && str[index] <= 90) {
+            return 1;
+        } else {
+            return 0;
+        }
+        index++;
+    }
+}
